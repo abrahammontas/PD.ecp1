4e66d0838e970b8bcaf575caea4cfedaecb50c30
Abraham Montas <abrahammontas@gmail.com>
2017-01-18 13:35:07 +0100

    Updating NaturalNumberEjecutable

--- src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEjecutable.java
+++ src/main/java/es/upm/miw/pd/factoryMethod/naturalNumber/NaturalNumberEjecutable.java
@@ -1,5 +1,9 @@
 package es.upm.miw.pd.factoryMethod.naturalNumber;
 
 public class NaturalNumberEjecutable {
-
+    public static void main(String[] args) {
+        NaturalNumberManager naturalNumberFactory= new NaturalNumberManager();
+        naturalNumberFactory.setNaturalNumberCreator(new NaturalNumberCreatorEs());
+        System.out.println(naturalNumberFactory.getNaturalNumber().getTextValue());;
+    }
 }
